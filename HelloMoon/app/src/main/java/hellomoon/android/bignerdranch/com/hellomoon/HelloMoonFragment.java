b147b175796be60aedc94dc053d861a941772af7
hasot <dima.yonkov@gmail.com>
2016-08-11 14:17:51 +0300

    Chapter 14 Audio+Video book: android programming Bryan Hardy and Bill Philips

--- HelloMoon/app/src/main/java/hellomoon/android/bignerdranch/com/hellomoon/HelloMoonFragment.java
+++ HelloMoon/app/src/main/java/hellomoon/android/bignerdranch/com/hellomoon/HelloMoonFragment.java
@@ -1,13 +1,13 @@
 package hellomoon.android.bignerdranch.com.hellomoon;
 
 
+
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -19,46 +19,72 @@ import android.widget.VideoView;
  */
 public class HelloMoonFragment extends Fragment {
 
-    private boolean play;
 
     private Button mPlayButton;
+    private Button mPauseButton;
     private Button mStopButton;
+    private Button mVideoButton;
 
     private AudioPlayer mPlayer = new AudioPlayer();
 
     private VideoView videoPlayer;
 
     @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent,
                              Bundle savedIstanceState){
         View v = inflater.inflate(R.layout.fragment_hello_moon, parent, false);
 
-        videoPlayer =  (VideoView)v.findViewById(R.id.videoPlayer);
+        mVideoButton = (Button)v.findViewById(R.id.hellomoon_videoButton);
+        videoPlayer = (VideoView) v.findViewById(R.id.videoPlayer);
         videoPlayer.setMediaController(new MediaController(this.getActivity()));
-        Uri myVideoUri= Uri.parse("android.resource://"  + "hellomoon.android.bignerdranch.com.hellomoon/raw/apollo_17_stroll");
+        Uri myVideoUri = Uri.parse("android.resource://" + "hellomoon.android.bignerdranch.com.hellomoon/raw/apollo_17_stroll");
         videoPlayer.requestFocus();
         videoPlayer.setVideoURI(myVideoUri);
-        videoPlayer.start();
+        mVideoButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
 
-        mPlayButton = (Button)v.findViewById(R.id.hellomoon_playButton);
-        mPlayButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                if (play) {
-                    mPlayer.pause();
-                    play = false;
-                    } else {
-                    mPlayer.play(getActivity());
-                    play = true;
-                }
+                videoPlayer.start();
+
             }
         });
 
-        mStopButton = (Button)v.findViewById(R.id.hellomoon_stopButton);
+
+
+        mPlayButton = (Button)v.findViewById(R.id.hellomoon_playButton);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                    mPlayer.play(getActivity());
+                    mPlayButton.setVisibility(View.INVISIBLE);
+                    mPauseButton.setVisibility(View.VISIBLE);
+
+
+            }
+        });
+
+        mPauseButton = (Button)v.findViewById(R.id.hellomoon_pauseButton);
+        mPauseButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mPlayer.pause();
+                mPauseButton.setVisibility(View.INVISIBLE);
+                mPlayButton.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+
+
+            mStopButton = (Button)v.findViewById(R.id.hellomoon_stopButton);
         mStopButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 mPlayer.stop();
-                play = false;
-
+                mPauseButton.setVisibility(View.INVISIBLE);
+                mPlayButton.setVisibility(View.VISIBLE);
             }
         });
         return v;
